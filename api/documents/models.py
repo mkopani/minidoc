c577bb9c96150da7c338a751d1377163f6981d7e
Mark Kopani <mkopani@gmail.com>
2025-01-09 08:34:17 -0800

    [api] Set up Django project w/ Document model

---
+++ api/documents/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+# from django.contrib.auth.models import User
+from django.db import models
+
+
+class Document(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # TODO
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # collaborators = models.ManyToManyField(User, related_name='collaborators')
