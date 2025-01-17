9e529a1add06c1525a22d7e85c03d4b09227cf8b
Mark Kopani <mkopani@gmail.com>
2025-01-17 10:39:45 -0800

    [web] Clean up models

--- web/src/models.ts
+++ web/src/models.ts
@@ -3,20 +3,11 @@
 export interface Document {
   id: string;
   title: string;
-  content: string;
+  readable_content: string;
   created_at: string;
   updated_at: string;
 }
 
 export interface DocumentsListPageResponse {
   documents: Document[];
-}
-
-// ********************************************************************
-// Users
-export interface User {
-  id: string;
-  username: string;
-  first_name: string;
-  last_name: string;
 }
